4927fdb488c811bd5e40f361fea13187461d0052
RobBlash <rob.blash@gmail.com>
2018-06-07 21:35:52 -0400

    added comments to code

--- resources/js/pannellumsource.js
+++ resources/js/pannellumsource.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    //get query values from URL based on parameters
+
+    //get pannellum parameters from URL query values, then populate source attribute of iframe
+
+    //get query value from URL based on parameter
     function getUrlVars() {
         var vars = {};
         var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -17,16 +20,22 @@ $(document).ready(function() {
       return urlparameter;
     }
 
-  //var myvar = getUrlParam('text','Empty')
 
-  var basePath = "./resources/pannellum/pannellum.htm?panorama=../../media/images/"
-  var imgName = getUrlParam('image','Empty')
-  var autoLoad = "&autoLoad=true"
-  var configs = "&config=nocompass.json"
-  var title = getUrlParam('title','')
-  var author = "&author=Robert Blash"
+  //populate variables with parameters needed for iframe source
+  var basePath = "./resources/pannellum/pannellum.htm?panorama=../../media/images/";
+  var autoLoad = "&autoLoad=true";
+  var configs = "&config=nocompass.json";
 
-  var srcStr = basePath + imgName + autoLoad + configs + "&title=" + title + author
-  $("#panoframe").attr("src",srcStr);
+  //parameters passed as URL query values
+  //var myvar = getUrlParam('parameter-name','value if not specified in URL')
+  var imgName = getUrlParam('image','Empty');
+  var title = "&title=" + getUrlParam('title','');
+  var author = "&author=" + getUrlParam('author','');
+
+  //concatenate variables into iframe source string
+  var srcStr = basePath + imgName + autoLoad + configs + title + author;
+
+  //use jquery to push source attribute to iframe
+  $("#pannellum-frame").attr("src",srcStr);
 
   });
